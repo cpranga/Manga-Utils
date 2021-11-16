0076800f1bdf0945b52e380c376a0cbfae022512
Chase Pranga <chase.pranga@tuta.io>
2021-11-16 16:03:47 -0600

    Add folder flag if albums should be folders instead of compressed files

--- create-album.py
+++ create-album.py
@@ -2,15 +2,17 @@ import os
 import argparse
 import zipfile
 from random import shuffle
+import shutil
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("root_dir", help="Directory containing manga folders to process.")
     parser.add_argument("-s", "--shuffle", help="Whether the album images should be shuffled.", action= "store_true")
     parser.add_argument("-r", "--reverse", help="Whether the album should have its images reversed", action="store_true")
-    parser.add_argument("-o", "--output", help="Directory to store albums", action="store_true")
+    parser.add_argument("-o", "--output", help="Directory to store albums")
     parser.add_argument("-n", "--nested", help= "Whether the source images are nested within a second level folder", action="store_true")
     parser.add_argument("-e", "--extension", help="Which extension to save the album as. Supported: 7z, cbz, zip")
+    parser.add_argument("-f", "--folder", help="Whether the albums should be extracted to a folder", action="store_true")
 
     args = parser.parse_args()
 
@@ -45,18 +47,31 @@ def process_folder(dir, args):
     if args.shuffle:
         shuffle(fullpaths)
 
-    with zipfile.ZipFile(os.path.join(args.output, dir_name + args.extension), 'w') as zip:
+    if args.folder:
         for file in fullpaths:
-            if os.path.isfile(file) == False or len(file.split(".")) != 2 or file.split(".")[1] == "zip":
-                continue
-            #print(os.path.join(args.output, str(counter) + "."+ file.split(".")[1]))
-            #shutil.move(file, os.path.join(args.output, str(counter) + "."+ file.split(".")[1]))
-            zip.write(file, str(counter) + "."+ file.split(".")[1])
-            if args.reverse:
-                counter -= 1
-            else:
-                counter += 1
-            #print(os.path.join(dir, file))
+                if os.path.isfile(file) == False or len(file.split(".")) != 2 or file.split(".")[1] == "zip":
+                    continue
+                if os.path.isdir(os.path.join(args.output, dir_name)) == False:
+                    os.mkdir(os.path.join(args.output, dir_name))
+                shutil.copy(file, os.path.join(args.output, dir_name, str(counter) + "."+ file.split(".")[1]))
+                if args.reverse:
+                    counter -= 1
+                else:
+                    counter += 1
+                #print(os.path.join(dir, file))
+    else:
+        with zipfile.ZipFile(os.path.join(args.output, dir_name + args.extension), 'w') as zip:
+            for file in fullpaths:
+                if os.path.isfile(file) == False or len(file.split(".")) != 2 or file.split(".")[1] == "zip":
+                    continue
+                #print(os.path.join(args.output, str(counter) + "."+ file.split(".")[1]))
+                #shutil.move(file, os.path.join(args.output, str(counter) + "."+ file.split(".")[1]))
+                zip.write(file, str(counter) + "."+ file.split(".")[1])
+                if args.reverse:
+                    counter -= 1
+                else:
+                    counter += 1
+                #print(os.path.join(dir, file))
         
 
 if __name__ == "__main__":
